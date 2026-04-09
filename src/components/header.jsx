1ded55376ccd7186ca1ed64fdec93755d289ae18
FarfadetMarginal <bapt935@gmail.com>
2026-04-09 12:31:24 +0200

    wip à l'aeroport

---
+++ src/components/header.jsx
@@ -0,0 +1,16 @@
+const Header = () => {
+    return (
+    <header>
+        <a className="logorelou" href="#">Carole <img src="./logovuhbjn.png" alt="logo" /> Fazi</a>
+        <div className="header1">
+            <img src="./epinglegauche.png" alt="epingle gauche cool" />
+            <a href="#">Prendre rendez-vous</a>
+            <a href="#">Les tarifs</a>
+            <a href="#">Où me trouver</a>
+            <img src="public/epingledroite.png" alt="epingle droite cool" />
+        </div>
+    </header>
+    )
+};
+
+export default Header;
